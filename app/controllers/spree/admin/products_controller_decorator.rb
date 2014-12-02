d985769a234820c11a74a512a19aaa669c706f91
Bharat <bh11.kumar@gmail.com>
2014-12-02 14:38:35 +0530

    Inventory management: Filter list by Product brand.

--- app/controllers/spree/admin/products_controller_decorator.rb
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -1,6 +1,6 @@
 module Spree
   Admin::ProductsController.class_eval do
-    skip_before_filter :load_resource, :only => [:edit_multiple, :update_multiple, :destroy_multiple]
+    skip_before_filter :load_resource, :only => [:edit_multiple, :update_multiple, :destroy_multiple, :display_brand_products]
     before_filter :load_multiple, :only => [:update_multiple, :destroy_multiple]
     before_filter :update_product_price, :only => :update_multiple
 
@@ -15,7 +15,23 @@ module Spree
       params[:q][:s] ||= 'name asc'
 
       @search = Product.where(:id => product_ids).ransack(params[:q])
-     # pagination_options = {:per_page => (session[:im_per_page] || 10), :page => params[:page]}
+      @collection = @search.result.page(params[:page]).per(session[:im_per_page] || 10)
+      
+      respond_with(@collection) do |format|
+        format.html
+        format.json { render :json => json_data }
+      end
+    end
+    
+    def display_brand_products
+      session[:im_per_page] = params[:per_page].to_i if !params[:per_page].nil?
+      @taxon = Taxon.root
+
+      params[:q] ||= {}
+      params[:q][:deleted_at_null] = '1' if params[:q][:deleted_at_null].nil?
+      params[:q][:s] ||= 'name asc'
+
+      @search = Product.with_property_value("brand", params[:id]).ransack(params[:q])
       @collection = @search.result.page(params[:page]).per(session[:im_per_page] || 10)
       
       respond_with(@collection) do |format|
